a9af17ec445160e35fb9d0d978043f19cd0fedfc
mukeshsahu1997 <sahumukesh095@gmail.com>
2022-08-25 22:36:35 +0530

    day3

---
+++ livescore.js
@@ -0,0 +1,403 @@
+let tEvents=[
+    {
+        status:"LIVE",
+        matches:"16th Match, Lord's, August 24, 2022",
+        team1:"WF-W",
+
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/317100/317110.jpg",
+        team2:"LS-W",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/317100/317119.png",
+        t1S:"(74/100 balls) 93/4",
+        t2S:"",
+        mstatus:"Fire chose to bat.",
+        cat:"women"
+      
+    },
+    {
+        status:"Result",
+        matches:"5th match (D/N), Al Amerat, August 24, 2022,",
+        team1:"SGP",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313144.logo.png",
+        team2:"KUW",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313158.logo.png",
+        t1S:"104",
+        t2S:"(78/120 balls) 105/4",
+        mstatus:"Kuwait won by 6 wickets (with 73 balls remaining)",
+        cat:"men"
+    },
+    {
+        status:"RESULT",
+        matches:"2nd ODI, Amstelveen, August 24, 2022, ",
+        team1:"IRE-W",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/156800/156800.png",
+        team2:"NL-W",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313136.logo.png",
+        t1S:"150",
+        t2S:"(94/120 balls) 130",
+        mstatus:"Ire Women won by 211 runs",
+        cat:"women"
+    },
+    {
+        status:"RESULT",
+        matches:"1st Match, Basseterre, August 24, 2022, ",
+        team1:"BR",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/325900/325974.png",
+        team2:"KR",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313140.logo.png",
+        t1S:"147",
+        t2S:"(19/20 ov, T:148) 149/2",
+        mstatus:"KR won by 8 wickets ",
+        cat:"men"
+
+    },
+    // {
+    //     status:"RESULT",
+    //     matches:"16th Match, Lord's, August 24, 2022, ",
+    //     team1:"Weish Fire-W",
+    //     t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/317100/317110.jpg",
+    //     team2:"London-W",
+    //     t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/317100/317119.png",
+    //     t1S:"119/9",
+    //     t2S:"(7.5/20 ov, T:105) 122/4",
+    //     mstatus:"Spirit won by 8 wickets ",
+    //     cat:"odi"
+    // },
+    {
+        status:"RESULT",
+        matches:"5th match (D/N), Al Amerat, August 24, 2022,  ",
+        team1:"Ireland W",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/156800/156800.png",
+        team2:"Neithterland W",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313136.logo.png",
+        t1S:"337/8",
+        t2S:"(33.2/50 ov, T:338) 127",
+        mstatus:"Ire Women won by 210 runs",
+        cat:"odi"
+
+    },
+    {
+        status:"TODAY, 7:00 PM",
+        matches:"Qualifier 2 (N), Bengaluru, August 25, 2022 ",
+        team1:"Mystics",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/343800/343854.png",
+        team2:"Mayusur w",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313400/313496.logo.png",
+        t1S:"",
+        t2S:"",
+        mstatus:"Match yet to begin",
+        cat:"t20s"
+    },
+    {
+        status:"TODAY, 8:00 PM",
+        matches:"17th Match, Southampton, August 25, 2022 ",
+        team1:"Barve",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/317100/317112.png",
+        team2:"Rocket",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/317100/317108.png",
+        t1S:"",
+        t2S:"",
+        mstatus:"Match yet to begin",
+        cat:"t20s"
+
+    },
+    {
+        status:"TODAY, 7:30 PM",
+        matches:"1st Match, Basseterre, August 25, 2022,  ",
+        team1:"Amazon",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313400/313482.logo.png",
+        team2:"Kings",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/325900/325975.png",
+        t1S:"",
+        t2S:"",
+        mstatus:"Match yet to begin",
+        cat:"test"
+    },
+    {
+        status:"TODAY, 11:30 PM",
+        matches:"25th Match (N), Southampton, August 25, 2022, ",
+        team1:"Patriots",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313500/313501.logo.png",
+        team2:"Tallawahs",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313400/313483.logo.png",
+        t1S:"",
+        t2S:"",
+        mstatus:"Match yet to begin",
+        cat:'test'
+
+    },
+    {
+        status:"TODAY, 11:30 PM",
+        matches:"28th Match (N), Southampton, August 25, 2022 ",
+        team1:"Kenya",
+        t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313146.logo.png",
+        team2:"Nepal",
+        t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/319900/319958.png",
+        t1S:"",
+        t2S:"",
+        mstatus:"Match yet to begin",
+        cat:"men",
+
+    },
+   
+
+
+];
+
+sponsoredArr=[
+    {
+        Image:"https://i.gadgets360cdn.com/large/intel-t_1661156344556.png",
+        title:"Get Superior Performance When You Need It the Most With 12th Gen Intel Core Mobile Processors",
+        mentor:"ketto",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/5j27g7o_image_120x90_19_August_22.jpg",
+        title:"Learn How To Protect Yourself From Inflation And Earn Additional Income",
+        mentor:"ketto",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://i.gadgets360cdn.com/large/a53_1660890777563.jpg",
+        title:"What Makes the Samsung Galaxy A53 5G the Perfect Smartphone When It Comes to Camera Performance",
+        mentor:"EMIDS",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/0oinabm8_image_120x90_09_August_22.jpg",
+        title:"Acing Hybrid Work 101: Here's How WeWork Can Help",
+        mentor:"NVEDA",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/u6ba8k6o_image_120x90_03_August_22.jpg",
+        title:"CRE8 Index: What Is It, And How Can It Help You Create A Great Crypto Portfolio?",
+        mentor:"Assetz",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://i.gadgets360cdn.com/large/sam-t_1659436979951.png",
+        title:"Samsung ‘Greater Than’ Galaxy Unpacked Event on August 10: Here’s What Can Be Expected to Unfold",
+        mentor:"Samsung",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/o8njplj8_computer-generic-usplash_120x90_01_August_22.jpg?im=FaceCrop,algorithm=dnn,width=240,height=180",
+        title:"How This Technology Can Bridge Language Divide In An Interconnected World",
+        mentor:"apple",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://i.gadgets360cdn.com/large/Huawei_Mate_Xs_2_1_1659003121363.jpg",
+        title:"HUAWEI Mate Xs 2 Outshines Its Competition",
+        mentor:"HUAWEI",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-07/01sqidno_myn-cab_120x90_01_July_22.jpg",
+        title:"Sponsored: MYn's 15,000 Cabs Take Bengaluru By Storm",
+        mentor:"MYn's",
+        sts:"Sponsored",
+    },
+]
+
+let topImage = [
+    'https://tpc.googlesyndication.com/simgad/931568015193886157',
+    'https://tpc.googlesyndication.com/simgad/10994949654465411539',
+    'https://tpc.googlesyndication.com/simgad/10926761020657163993'
+]
+
+let TopImg = () =>{
+    let rand = Math.floor(Math.random()*3)
+    document.getElementById("abtopimage").src = topImage[rand];
+}
+
+TopImg()
+
+let array = ["All", "t20s", "odi", "test","men","women"];
+let btndiv = document.createElement("div");
+btndiv.setAttribute("id", "btndiv");
+
+
+for (let i = 0; i < array.length; i++) {
+    let a = document.createElement("button");
+    a.setAttribute("id", "sortbtn");
+    a.innerText = array[i];
+    a.addEventListener("click", function (event) {
+      
+        if (event.target.innerText == "All") {
+           displayScore(tEvents)
+           
+          
+        } else {
+            let filtered = tEvents.filter(function (ele) {
+                return ele.cat == event.target.innerText;
+            });
+
+           
+
+            displayScore(filtered)
+          
+          
+           
+         }
+    })
+    btndiv.append(a)
+    document.getElementById("cat").append(btndiv)
+}
+
+
+
+
+let displayScore=(tEvents)=>{
+
+    //let matches = document.getElementById("matches");  
+  
+
+    // {
+    //     status:"TODAY, 11:30 PM",
+    //     matches:"28th Match (N), Southampton, August 25, 2022 ",
+    //     team1:"Kenya",
+    //     t1i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/313100/313146.logo.png",
+    //     team2:"Nepal",
+    //     t2i:"https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_80/lsci/db/PICTURES/CMS/319900/319958.png",
+    //     t1S:"",
+    //     t2S:"",
+    //     mstatus:"Match yet to begin",
+    //     cat:"men",
+
+    // },
+    
+    document.getElementById("matches").innerHTML = null;
+  tEvents.forEach((elem)=>{
+    let box=document.createElement("div");
+    box.setAttribute("class","box")
+    
+    let status=document.createElement("p");
+   
+    status.innerText=elem.status;
+    if(elem.status=="LIVE"){
+        status.style.color="red"
+        status.style.fontWeight="bold"
+    }
+    else{
+        status.style.color="grey"
+        status.style.fontWeight="bold"
+
+    }
+
+
+
+    let matches=document.createElement("p");
+    matches.innerText=elem.matches;
+
+    let fdiv=document.createElement("div");
+    fdiv.setAttribute("class","fdiv")
+
+    let t1i=document.createElement("img");
+    t1i.src=elem.t1i;
+
+    let team1=document.createElement("h5");
+    team1.innerText=elem.team1;
+
+    let t1d=document.createElement("div");
+    t1d.setAttribute("class","t1d")
+    t1d.append(t1i,team1)
+
+    let t1S=document.createElement("h5");
+    // if(elem.t1S.length!=0){
+    //     t1S.innerText=elem.t1S;
+    // }
+    // else{
+    //     t1S.innerText=0
+    // }
+    
+    
+
+    fdiv.append(t1d,t1S);
+
+    let sdiv=document.createElement("div");
+    sdiv.setAttribute("class","sdiv")
+
+    let t2i=document.createElement("img");
+    t2i.src=elem.t2i;
+
+    let team2=document.createElement("h5");
+    team2.innerText=elem.team2;
+
+    let t2d=document.createElement("div");
+    t2d.setAttribute("class","t2d")
+    t2d.append(t2i,team2)
+
+    let t2S=document.createElement("h5");
+    t2S.innerText=elem.t2S;
+
+    sdiv.append(t2d,t2S);
+
+    let mstatus=document.createElement("p");
+    mstatus.innerText=elem.mstatus
+    
+
+   let bndiv=document.createElement("div");
+
+   let bt1=document.createElement("p");
+   bt1.innerText="Summary";
+
+   let bt2=document.createElement("p");
+   bt2.innerText="Series Home"
+
+   bndiv.append(bt1,bt2);
+   bndiv.setAttribute("class","bndiv")
+
+   box.append(status,matches,fdiv,sdiv,mstatus,bndiv)
+
+   document.getElementById("matches").append(box);
+
+  
+    });
+};
+
+displayScore(tEvents)
+
+
+
+// {
+//     Image:"https://c.ndtvimg.com/2022-07/01sqidno_myn-cab_120x90_01_July_22.jpg",
+//     title:"Sponsored: MYn's 15,000 Cabs Take Bengaluru By Storm",
+//     mentor:"MYn's",
+//     sts:"Sponsored",
+// },
+
+let displayNews=(sponsoredArr)=>{
+    document.getElementById("sponsor").innerHTML=null;
+  sponsoredArr.forEach((elem)=>{
+
+    let box=document.createElement("div");
+    box.setAttribute("class","nbox");
+
+    let image=document.createElement("img");
+    image.src=elem.Image;
+
+    let title=document.createElement("h3");
+    title.innerText=elem.title;
+
+    let mdiv=document.createElement("div");
+    mdiv.setAttribute("class","mbox")
+
+    let mentor= document.createElement("p");
+    mentor.innerText=elem.mentor;
+
+    let sts=document.createElement("p");
+    sts.innerText=elem.sts;
+
+    mdiv.append(mentor,sts);
+
+    box.append(image,title,mdiv);
+
+    document.getElementById("sponsor").append(box)
+
+  })
+
+
+}
+
+displayNews(sponsoredArr)
