2504bd5bacb087bcfb6ad954c675ac8afcd0230e
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2022-08-26 16:03:24 +0530

    news page work done with footer

---
+++ components/footer.js
@@ -0,0 +1,67 @@
+let footer = () =>{
+    return `<div class="f_footer">
+    <div class="fkey">
+        <h2>Key Series</h2>
+        <h2>Quick Links</h2>
+        <h2>ESPNcricinfo Apps</h2>
+        <h2> Follow ESPNcricinfo</h2>
+        <h2>ESPN Sites</h2>
+    </div>
+    <!-- keys ending header -->
+    <div id="table">
+        <!-- coloumn 1 data  -->
+        <div class="coloumn01"> 
+          <a href="#"> <p>Asian Cup QLF</p> </a>
+          <a href="#"> <p>Asia Cup</p> </a>
+          <a href="#"> <p>Asia CupEngland v SouthAfrica</p> </a>
+          <a href="#"> <p>Australia v Zimbabwe</p> </a>
+          <a href="#"> <p>The Hundred (M)</p> </a>
+          <a href="#"> <p>The Hundred (W) </p> </a>
+          <a href="#"> <p>County Div1 </p> </a>
+          <a href="#"> <p>County Div2</p> </a>
+          <a href="#"> <p> 6xity (M)</p> </a>
+          <a href="#"> <p>6xity (W)</p> </a>
+          <a href="#"> <p>Women's Championship</p> </a>
+          <a href="#"> <p>World Test Championship</p> </a>
+          <a href="#"> <p>World Cup Super League</p> </a> 
+        </div>
+            <!-- coloumn 2 data  -->
+        <div class="coloumn01">
+            <a href="#"> <p>T20 Time Out</p> </a>
+            <a href="#"> <p>T20 Time Out Hindi</p> </a>
+            <a href="#"> <p>ICC Rankings</p> </a>
+            <a href="#"> <p>Fantasy Pick</p> </a>
+            <a href="#"> <p>Haan Ya Naa</p> </a>
+        </div>
+        <!-- coloumn -3 data -->
+        <div class="coloumn01">
+            <a href="#"> <p>Android App</p> </a>
+            <a href="#"> <p> iOS App</p> </a>
+        </div>
+             <!-- coloumn -4 data -->
+        <div class="coloumn03">
+            <a href="#"> <p>Instagram</p> </a>
+            <a href="#"> <p>Twitter</p> </a>
+            <a href="#"> <p>Facebook</p> </a>
+            <a href="#"> <p>YouTube</p> </a>
+        </div>
+             <!-- coloumn -5 data -->
+        <div class="coloumn04">
+            <a href="#"> <p>The Cricket Monthly</p> </a>
+            <a href="#"><p>ESPN</p></a>
+        </div>
+    </div>
+   
+</div>
+</div>
+<!-- end of page  -->
+<div id="endfoot">
+<div>
+<p class="endnote1">Trems of Use | Privacy Policy | Intrest-Based Ads | Addendum to the Global Privacy Policy | Feedback</p>
+</div>
+<div>
+    <p class="endnote2"> @2022 ESPN Sports Media Ltd. All rights reserved</p>
+</div>
+</div>`
+}
+export default footer();
---
+++ scripts/news.js
@@ -0,0 +1,108 @@
+import navbar from "../components/navbar.js";
+import stickynavbar from "../components/navbarsticky.js";
+import footer from "../components/footer.js";
+
+document.getElementById('navbar').innerHTML =navbar;
+document.getElementById('stickynavbar').innerHTML = stickynavbar;
+document.querySelector('footer').innerHTML = footer;
+
+let data = JSON.parse(localStorage.getItem("article"))
+console.log(data)
+let displayData = () =>{
+    let heading = document.createElement("h1");
+    heading.innerText = data.title;
+    let meta = document.createElement("p");
+    meta.innerText = data.metaDesc;
+    let author = document.createElement("h5");
+    author.innerHTML = `Author:- ${data.author}`;
+    let postor = document.createElement("img");
+    postor.src = data.image;
+    let div = document.createElement("div");
+    div.innerHTML = data.content;
+    document.getElementById("abhiRightContainer").append(heading,meta,author,postor,div);
+}
+
+let sponsoredArr=[
+    {
+        Image:"https://i.gadgets360cdn.com/large/intel-t_1661156344556.png",
+        title:"Get Superior Performance When You Need It the Most With 12th Gen Intel Core Mobile Processors",
+        mentor:"ketto",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/5j27g7o_image_120x90_19_August_22.jpg",
+        title:"Learn How To Protect Yourself From Inflation And Earn Additional Income",
+        mentor:"ketto",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://i.gadgets360cdn.com/large/a53_1660890777563.jpg",
+        title:"What Makes the Samsung Galaxy A53 5G the Perfect Smartphone When It Comes to Camera Performance",
+        mentor:"EMIDS",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/0oinabm8_image_120x90_09_August_22.jpg",
+        title:"Acing Hybrid Work 101: Here's How WeWork Can Help",
+        mentor:"NVEDA",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/u6ba8k6o_image_120x90_03_August_22.jpg",
+        title:"CRE8 Index: What Is It, And How Can It Help You Create A Great Crypto Portfolio?",
+        mentor:"Assetz",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://i.gadgets360cdn.com/large/sam-t_1659436979951.png",
+        title:"Samsung ‘Greater Than’ Galaxy Unpacked Event on August 10: Here’s What Can Be Expected to Unfold",
+        mentor:"Samsung",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-08/o8njplj8_computer-generic-usplash_120x90_01_August_22.jpg?im=FaceCrop,algorithm=dnn,width=240,height=180",
+        title:"How This Technology Can Bridge Language Divide In An Interconnected World",
+        mentor:"apple",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://i.gadgets360cdn.com/large/Huawei_Mate_Xs_2_1_1659003121363.jpg",
+        title:"HUAWEI Mate Xs 2 Outshines Its Competition",
+        mentor:"HUAWEI",
+        sts:"Sponsored",
+    },
+    {
+        Image:"https://c.ndtvimg.com/2022-07/01sqidno_myn-cab_120x90_01_July_22.jpg",
+        title:"Sponsored: MYn's 15,000 Cabs Take Bengaluru By Storm",
+        mentor:"MYn's",
+        sts:"Sponsored",
+    },
+]
+
+let displayNews= () =>{
+    document.getElementById("sponsor").innerHTML=null;
+    sponsoredArr.forEach((elem)=>{
+        let box=document.createElement("div");
+        box.setAttribute("class","nbox");
+        let image=document.createElement("img");
+        image.src=elem.Image;
+        let title=document.createElement("h3");
+        title.innerText=elem.title;
+        let mdiv=document.createElement("div");
+        mdiv.setAttribute("class","mbox")
+        let mentor= document.createElement("p");
+        mentor.innerText=elem.mentor;
+        let sts=document.createElement("p");
+        sts.innerText=elem.sts;
+        mdiv.append(mentor,sts);
+        box.append(image,title,mdiv);
+        document.getElementById("sponsor").append(box)
+    })
+}
+
+
+window.onload = () =>{
+    displayData();
+    displayNews();
+    // localStorage.clear();
+}
